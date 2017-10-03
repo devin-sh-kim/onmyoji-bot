5c35a8ee4093b45fd9fce5cc8700149de24c2b28
Sanghoon Kim <ujacha.factory@gmail.com>
2017-10-03 15:48:25 +0900

    update getMapping

--- src/main/java/net/ujacha/onmyojibot/kakao/MainController.java
+++ src/main/java/net/ujacha/onmyojibot/kakao/MainController.java
@@ -1,12 +1,12 @@
 package net.ujacha.onmyojibot.kakao;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
-	@GetMapping("/keyboard")
+	@RequestMapping("/keyboard")
 	public KeyboardResponse keyboard() {
 		KeyboardResponse keyboardResponse = new KeyboardResponse();
 		
