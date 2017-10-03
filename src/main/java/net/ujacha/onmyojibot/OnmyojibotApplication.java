c518bde2d98454d5b0aa6cd9788fa6fd4641c8b1
Sanghoon Kim <ujacha.factory@gmail.com>
2017-10-03 16:10:11 +0900

    springboot update

--- src/main/java/net/ujacha/onmyojibot/OnmyojibotApplication.java
+++ src/main/java/net/ujacha/onmyojibot/OnmyojibotApplication.java
@@ -2,9 +2,16 @@ package net.ujacha.onmyojibot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OnmyojibotApplication {
+public class OnmyojibotApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OnmyojibotApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnmyojibotApplication.class, args);
--- src/main/java/net/ujacha/onmyojibot/kakao/MainController.java
+++ src/main/java/net/ujacha/onmyojibot/kakao/MainController.java
@@ -1,12 +1,14 @@
 package net.ujacha.onmyojibot.kakao;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("kakao")
 public class MainController {
 
-	@RequestMapping("/keyboard")
+	@GetMapping("keyboard")
 	public KeyboardResponse keyboard() {
 		KeyboardResponse keyboardResponse = new KeyboardResponse();
 		
