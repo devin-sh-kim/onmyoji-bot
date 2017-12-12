c1899a15708338e2cea5cdf915441cdae375f376
Sanghoon Kim <ujacha.factory@gmail.com>
2017-12-12 23:43:25 +0900

    add web ui

--- src/main/resources/public/app.js
+++ src/main/resources/public/app.js
@@ -2,76 +2,81 @@ $(function(){
 
     // $('.ui.sticky').sticky();
 
-    $("#input-query").on("keyup", function(event){
+    $("#input-query").on("keypress", function(event){
         if(event.keyCode == 13){
-            $("#btn-query").trigger("click");
+            sendQuery();
+            event.preventDefault();
         }
     });
 
     $("#btn-query").on("click", function() {
-        var query = $.trim($("#input-query").val());
-
-        if(query == ""){
-            var $query = $("#input-query");
-            $query.val("");
-            return false;
-        }
-
-        var data = {
-            user_key: "WEB_USER",
-            type: "text",
-            content: query
-        };
-        $.ajax({
-            url:"kakao/message",
-            method: "POST",
-            data: JSON.stringify(data),
-            headers: {
-                "Content-Type": "application/json; charset=utf-8",
-                "Cache-Control": "no-cache"
-            },
-        }).done(function(res){
-            console.log(res);
-            var $query = $("#input-query");
-            $query.val("");
-
-
-            var $result = $("#result");
-            var $message = $("<div class='message ui piled yellow segment'></div>");
-            if(res.message.photo){
-                $message.append("<image src='"+res.message.photo.url+"'/>");
-            }
-            $message.append("<pre style='color: black'>"+res.message.text+"</pre>");
-
-            var keyboardType = res.keyboard.type;
-
-            switch (keyboardType) {
-                case "text":
-                    break;
-                case "buttons":
-                    var buttons = res.keyboard.buttons;
-                    var $buttons = $("<div class=''></div>");
-                    $.each(buttons, function(i, item){
-                        // console.log(i, item);
-                        $buttons.append("<button class='ui mini blue button btn-select-query' value='"+item+"'>"+item+"</button>");
-                    });
-                    $message.append($buttons);
-                    break;
-            }
-
-
-            console.log($message);
-
-            $result.prepend($message);
-
-        });
+        sendQuery();
     });
 
     $("#result").on("click", ".btn-select-query", function(){
         var $this = $(this);
         var query = $this.val();
         $("#input-query").val(query);
-        $("#btn-query").trigger("click");
+        sendQuery();
     });
 
 });
+
+function sendQuery(){
+    var query = $.trim($("#input-query").val());
+
+    if(query == ""){
+        var $query = $("#input-query");
+        $query.val("");
+        return false;
+    }
+
+    var data = {
+        user_key: "WEB_USER",
+        type: "text",
+        content: query
+    };
+    $.ajax({
+        url:"kakao/message",
+        method: "POST",
+        data: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            "Cache-Control": "no-cache"
+        },
+    }).done(function(res){
+        console.log(res);
+        var $query = $("#input-query");
+        $query.val("");
+
+
+        var $result = $("#result");
+        var $message = $("<div class='message ui piled yellow segment'></div>");
+        if(res.message.photo){
+            $message.append("<image src='"+res.message.photo.url+"'/>");
+        }
+        $message.append("<pre style='color: black'>"+res.message.text+"</pre>");
+
+        var keyboardType = res.keyboard.type;
+
+        switch (keyboardType) {
+            case "text":
+                break;
+            case "buttons":
+                var buttons = res.keyboard.buttons;
+                var $buttons = $("<div class=''></div>");
+                $.each(buttons, function(i, item){
+                    // console.log(i, item);
+                    $buttons.append("<button class='ui mini blue button btn-select-query' value='"+item+"'>"+item+"</button>");
+                });
+                $message.append($buttons);
+                break;
+        }
+
+
+        console.log($message);
+
+        $result.prepend($message);
+
+    });
+}
