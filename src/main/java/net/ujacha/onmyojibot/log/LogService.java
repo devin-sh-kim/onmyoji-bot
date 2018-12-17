522957f159f9e4d4680fed180c632312c3acd3b1
devin <devin.sh.kim@gmail.com>
2018-12-17 20:30:50 +0900

    로깅 테이블 구조 변경

--- src/main/java/net/ujacha/onmyojibot/log/LogService.java
+++ src/main/java/net/ujacha/onmyojibot/log/LogService.java
@@ -5,6 +5,8 @@ import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import net.ujacha.onmyojibot.aws.DynamoDBClientFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,26 +16,33 @@ import java.util.Date;
 @Service
 public class LogService {
 
-    private static final String LOG_TABLE_NAME = "OMJ_LOG_NOT_FOUND";
+    private static final Logger log = LoggerFactory.getLogger(LogService.class);
+
+    private static final String LOG_TABLE_NAME = "OMJ_LOG";
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMddHHmmss");
 
 
     @Autowired
     private DynamoDBClientFactory dynamoDBClientFactory;
 
-    public void logNotFound(String userKey, String query){
+    public void logNotFound(String userKey, String query) {
+        try {
+            AmazonDynamoDB client = dynamoDBClientFactory.createClient();
+            DynamoDB dynamoDB = new DynamoDB(client);
 
-        AmazonDynamoDB client = dynamoDBClientFactory.createClient();
-        DynamoDB dynamoDB = new DynamoDB(client);
+            Table table = dynamoDB.getTable(LOG_TABLE_NAME);
 
-        Table table = dynamoDB.getTable(LOG_TABLE_NAME);
+            String now = DATE_FORMAT.format(new Date());
+            Item item = new Item()
+                    .withPrimaryKey("logType", "NOT_FOUND")
+                    .withString("createAt", now)
+                    .withString("userKey", userKey)
+                    .withString("query", query);
+            table.putItem(item);
 
-        String now = DATE_FORMAT.format(new Date());
-        Item item = new Item()
-                .withPrimaryKey("userKey", userKey)
-                .withString("createAt", now)
-                .withString("query", query);
-        table.putItem(item);
+        }catch (Exception e){
+            log.error("{}", e.getMessage());
+        }
 
 
     }
